3090a785ccdc5eab0ed2ce2799354db5150fe4c2
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-03-02 20:09:14 +0900

    Minimised source code using functions and got the app done

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,52 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note1.wav'));
-              },
-              child: Text('Click Me'),
-            ),
-          ),
+          child: Tones(),
         ),
       ),
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Tones extends StatefulWidget {
+  @override
+  State<Tones> createState() => _TonesState();
+}
+
+class _TonesState extends State<Tones> {
+  void playSound(int number) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$number.wav'));
+  }
+
+  Expanded buildKey(Color color, int soundNumber) => Expanded(
+        child: FilledButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll<Color>(color),
+          ),
+          onPressed: () {
+            playSound(soundNumber);
+          },
+          child: Container(),
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        buildKey(Colors.red, 1),
+        buildKey(Colors.orange, 2),
+        buildKey(Colors.yellow, 3),
+        buildKey(Colors.lightGreen, 4),
+        buildKey(Colors.green, 5),
+        buildKey(Colors.blue, 6),
+        buildKey(Colors.purple, 7),
+      ],
     );
   }
 }
